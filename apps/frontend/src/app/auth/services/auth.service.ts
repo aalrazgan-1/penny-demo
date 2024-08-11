2ebfdddd17b4691b21597afd6dd6bcdb3fe95be7
Abdullah Alrazgan <aalrazga@cisco.com>
2024-08-11 02:11:34 -0400

    [Backend] Remove unused functions in auth service

--- apps/frontend/src/app/auth/services/auth.service.ts
+++ apps/frontend/src/app/auth/services/auth.service.ts
@@ -10,19 +10,11 @@ export class AuthService {
 
   constructor(private http: HttpClient) {}
 
-  signup(user: { email: string; password: string; firstName: string; lastName: string }): Observable<any> {
-    return this.http.post<any>(`${this.apiUrl}/signup`, user);
-  }
-
   login(email: string, password: string): Observable<{ access_token: string, expiresAt: any }> {
     return this.http.post<{ access_token: string, expiresAt: any }>(`${this.apiUrl}/login`, { email, password });
   }
 
   getProfile(): Observable<{ userId: string; email: string }> {
     return this.http.get<{ userId: string; email: string }>(`${this.apiUrl}/profile`);
-  }
-
-  checkEmail(email: string): Observable<{ isDuplicate: boolean }> {
-    return this.http.get<{ isDuplicate: boolean }>(`${this.apiUrl}/check-email?email=${email}`);
   }
 }
